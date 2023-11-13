c505fd01efcdfdd9668139fc5fcf9732e010b395
YousifMHelal <yousifM.Helal@gmail.com>
2023-11-13 23:50:11 +0200

    Creating Api

--- src/components/cardList/CardLIst.jsx
+++ src/components/cardList/CardLIst.jsx
@@ -3,7 +3,20 @@ import styles from "./cardList.module.css";
 import Pagination from "../pagination/Pagination";
 import Card from "../card/Card";
 
-const CardList = () => {
+const getDate = async () => {
+  const res = await fetch("http://localhost:3000/api/posts", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("failed to fetch");
+  }
+
+  return res.json();
+};
+
+const CardList = async () => {
+  const data = await getDate()
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Resent Posts </h1>
--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -3,83 +3,43 @@ import styles from "./categoryList.module.css";
 import Link from "next/link";
 import Image from "next/image";
 
-const CategoryList = () => {
+const getDate = async () => {
+  const res = await fetch("http://localhost:3000/api/categories", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("failed to fetch");
+  }
+
+  return res.json();
+};
+
+const CategoryList = async () => {
+  const data = await getDate();
+  console.log(data);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>popular category</h1>
       <div className={styles.categories}>
-        <Link
-          href="/blog?/cat=style"
-          className={`${styles.category} ${styles.style}`}>
-          <Image
-            src="/p1.jpeg"
-            alt="style"
-            width={32}
-            height={32}
-            className={styles.img}
-          />
-          style
-        </Link>
-        <Link
-          href="/blog?/cat=style"
-          className={`${styles.category} ${styles.style}`}>
-          <Image
-            src="/p1.jpeg"
-            alt="style"
-            width={32}
-            height={32}
-            className={styles.img}
-          />
-          style
-        </Link>
-        <Link
-          href="/blog?/cat=style"
-          className={`${styles.category} ${styles.style}`}>
-          <Image
-            src="/p1.jpeg"
-            alt="style"
-            width={32}
-            height={32}
-            className={styles.img}
-          />
-          style
-        </Link>
-        <Link
-          href="/blog?/cat=style"
-          className={`${styles.category} ${styles.style}`}>
-          <Image
-            src="/p1.jpeg"
-            alt="style"
-            width={32}
-            height={32}
-            className={styles.img}
-          />
-          style
-        </Link>
-        <Link
-          href="/blog?/cat=style"
-          className={`${styles.category} ${styles.style}`}>
-          <Image
-            src="/p1.jpeg"
-            alt="style"
-            width={32}
-            height={32}
-            className={styles.img}
-          />
-          style
-        </Link>
-        <Link
-          href="/blog?/cat=style"
-          className={`${styles.category} ${styles.style}`}>
-          <Image
-            src="/p1.jpeg"
-            alt="style"
-            width={32}
-            height={32}
-            className={styles.img}
-          />
-          style
-        </Link>
+        {data.map((item) => (
+          <Link
+            key={item.id}
+            href="/blog?/cat=style"
+            className={`${styles.category} ${styles[item.slug]}`}>
+            {item.img && (
+              <Image
+                src={item.img}
+                alt="style"
+                width={32}
+                height={32}
+                className={styles.img}
+              />
+            )}
+            {item.title}
+          </Link>
+        ))}
       </div>
     </div>
   );
